c22b4ef0102096d19b7234d13e183f2334a7bd1f
CALVINCE <calvince.ogwel@gmail.com>
2023-02-23 15:34:17 +0300

    completed compulsory lab

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 2023_02_23_122845) do
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.integer "grade"
+    t.datetime "birth_date"
+  end
 
 end
